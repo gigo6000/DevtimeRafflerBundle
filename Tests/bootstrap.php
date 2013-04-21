ac314e7f04e3d0907840171af27eb2464898c91a
Carlos Mafla <gigo6000@hotmail.com>
2013-04-21 16:35:44 -0500

    Add AppKernel for Functional tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,14 +1,11 @@
 <?php
 
-function includeIfExists($file)
-{
-    if (file_exists($file)) {
-        return include $file;
-    }   
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    throw new \RuntimeException('Install the dependencies to run the test suite.');
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../../../autoload.php'))) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
-}
+$loader = require $file;
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
